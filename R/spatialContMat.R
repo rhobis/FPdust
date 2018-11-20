beb3d62a37a1ba897ec8680a125c4bee6de3449e
Roberto S <rhobis@gmail.com>
2018-11-20 14:55:45 +0100

    more efficient implementation

--- R/spatialContMat.R
+++ R/spatialContMat.R
@@ -1,21 +1,35 @@
 #' Create a spatial contingency matrix from a list of contingencies
 #'
-#' Given a \code{list} of contingencies for each unit in a population of size \code{N},
-#' \code{scmat} creates a matrix of local contingency matrix of size \code{NxN}
-#' of 1's (if units i and j are contiguous) and 0's (otherwise).
+#' Create a matrix of local contingency matrix of size \code{NxN} starting from
+#' a list.
+#'
 #'
 #' @param clist list of length N in which each element is a vector of id's continguous units
-#' 
+#'
+#'
+#' @details
+#' The input argument \code{clist} should be a list of lenght N. Each element of
+#' this list must be a numeric vector, including the indices of the units that
+#' are contiguous to the unit corresponding to the list element.
+#' Function \code{spatialContingency} returns a matrix whose values are 1's (contiguous units)
+#' and 0's (non-contiguous units).
+#'
 #' @export
+#'
 
 
 spatialContMat <- function(clist){
+
     if(!is.list(clist)) stop('the argument should be a list of length N')
     N <- length(clist)
 
-    out <- matrix(0,N,N)
-    for(i in 1:N){
-        out[i, clist[[i]] ] <- 1
-    }
+    out <- lapply(seq_len(N), function(i) {
+        v <- rep(0, N)
+        v[ clist[[i]] ] <- 1
+        return(v)
+    } )
+    out <- Reduce(rbind, out)
+    diag(out) <- 0
+
     return(out)
 }
